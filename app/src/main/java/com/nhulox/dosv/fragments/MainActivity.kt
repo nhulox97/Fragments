e6e32c5225a0917e35c9ed95ce03564982aa7a53
nhulox97 <sergiobernal909@gmail.com>
2019-04-25 21:00:41 -0600

    Adding the 3rd fragment

--- app/src/main/java/com/nhulox/dosv/fragments/MainActivity.kt
+++ app/src/main/java/com/nhulox/dosv/fragments/MainActivity.kt
@@ -9,9 +9,12 @@ import android.widget.Toast
 class MainActivity : AppCompatActivity() {
 
     private lateinit var btnFragment: Button
+
     private lateinit var fragment1: Fragment1
     private lateinit var fragment2: Fragment2
-    private var ACTUAL_FRAGMENT = true
+    private lateinit var fragment3: Fragment3
+
+    private var ACTUAL_FRAGMENT = 0
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,8 +22,10 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         btnFragment = findViewById(R.id.btnFragment)
+
         fragment1 = Fragment1()
         fragment2 = Fragment2()
+        fragment3 = Fragment3()
 
         btnFragment.setOnClickListener{
             insertFragment(it)
@@ -32,17 +37,25 @@ class MainActivity : AppCompatActivity() {
         //Toast.makeText(this, "Changing Fragments", Toast.LENGTH_LONG).show()
         val manager = supportFragmentManager
         val transaction = manager.beginTransaction()
-        if (ACTUAL_FRAGMENT){
+        if (ACTUAL_FRAGMENT == 0){
             transaction.replace(R.id.container, fragment1)
             transaction.addToBackStack(null)
             transaction.commit()
-            ACTUAL_FRAGMENT = false
+            ACTUAL_FRAGMENT++
         }
-        else{
+
+        else if (ACTUAL_FRAGMENT == 1){
             transaction.replace(R.id.container, fragment2)
             transaction.addToBackStack(null)
             transaction.commit()
-            ACTUAL_FRAGMENT = true
+            ACTUAL_FRAGMENT++
+        }
+
+        else if (ACTUAL_FRAGMENT == 2){
+            transaction.replace(R.id.container, fragment3)
+            transaction.addToBackStack(null)
+            transaction.commit()
+            ACTUAL_FRAGMENT = 0
         }
 
     }
